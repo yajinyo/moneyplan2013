0bfab44270da9d2381e45b6ccb831a117634502d
yajin@tpj <yajin@tpj>
2013-11-15 10:11:20 +0900

    2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneyplan2013::Application.config.secret_token = 'e848c841f756a9a5d58bb2e3bd02d0b8664b185ff8d4f9815912097c619be02e2da26809dbe006d2f17a662936e81e5dbfd050bcdce3df85cd27cb2baacb774c'
